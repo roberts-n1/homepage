578299d6e5eefa375f570adc2dd7cda4bb4f68f8
Roberts Neimanis <robertsn123123@gmail.com>
2020-09-13 13:34:29 +0300

    updated rubocop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# pages controller
 class PagesController < ApplicationController
   def home; end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module HomePage
-  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
